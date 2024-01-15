0ccb62c50f922ab9d60cc0c5d2626e6c5ff2bc2f
Tech-willson20 <wafoakwa9@gmail.com>
2024-01-15 12:40:07 -0800

    New work

---
+++ test02array/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {10, 20, 30, 40, 50}; // declare and initialize an array of integers
+    for (int i = 0; i < 5; i++) { // loop through the array
+        cout << "Element " << i << " of the array is: " << arr[i] << endl;
+    }
+    return 0;
+}
