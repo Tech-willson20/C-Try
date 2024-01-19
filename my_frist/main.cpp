1888ee044c532f5fefd7b0cc316b2ee6f7422e0d
Tech-willson20 <wafoakwa9@gmail.com>
2024-01-19 22:17:34 +0100

    New force

---
+++ my_frist/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+    float number;
+
+    int Age;
+    bool name;
+    char b;
+    string list;
+    double index;
+string list="William Afoakwa";
+char b="A";
+bool name="True";
+number=2.999;
+donble index=2932.39239;
+
+
+cout<<number<<endl;
+cout<<index<<endl;
+cout<<b<<endl;
+cout<<Age<<endl;
+cout<<name<<endl;
+cout<<list<<endl;
+
+return 0;
+}
+
