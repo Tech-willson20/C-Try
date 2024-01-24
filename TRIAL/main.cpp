f48f4c1a424c5ab26f9f62244dfab0738841bf3c
Tech-willson20 <wafoakwa9@gmail.com>
2024-01-24 03:31:05 +0100

    new force

---
+++ TRIAL/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    float radius;
+    cout << "Enter your radius" << endl;
+    cin >>radius;
+
+    float pi ;
+    pi=3.146;
+
+
+
+    float area;
+area = pi + radius;
+    cout<<"The area is "<< area;
+    return 0;
+}
